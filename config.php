91603634e67e7a341782cb5c77577a46da009d25
IdkBored-d <DevinbHe@gmail.com>
2024-12-03 17:06:31 -0500

    fix

--- config.php
+++ config.php
@@ -1,24 +1,4 @@
 <?php
-//    const DB_SERVER = "localhost";
-//    const DB_USER = "root";
-//    const DB_PASSWORD = "root";
-//    const DB_NAME = "finalproject";
-//
-//    function getDB(){
-//        try{
-//            $pdo = new PDO("mysql:host=" . DB_SERVER .
-//                ";dbname=" . DB_NAME,
-//                DB_USER,
-//                DB_PASSWORD);
-//            $pdo->setAttribute(PDO::ATTR_ERRMODE,
-//                PDO::ERRMODE_EXCEPTION);
-//            return $pdo;
-//        }
-//        catch(PDOException $e){
-//            echo $e->getMessage();
-//        }
-//    }
-
 $db_host = 'localhost';
 $db_user = 'root';
 $db_password = 'root';
